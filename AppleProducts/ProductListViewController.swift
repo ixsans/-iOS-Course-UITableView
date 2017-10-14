45968ba7c8ab865b5dc3b86261b114998639bb28
Ikhsanudin Hakim <ikhsanudinhakim@gmail.com>
2017-10-14 09:34:30 +0700

    Add editing action

--- AppleProducts/ProductListViewController.swift
+++ AppleProducts/ProductListViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ProductListViewController: UITableViewController {
 
-    var products = ProductLine.getProductLines()[1].products
+    var productLines = ProductLine.getProductLines()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.estimatedRowHeight = tableView.rowHeight
         tableView.rowHeight = UITableViewAutomaticDimension
+        
+        navigationItem.rightBarButtonItem = editButtonItem
     }
 
     
@@ -24,20 +26,29 @@ class ProductListViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return productLines.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return products.count
+        return productLines[section].products.count
     }
-
-    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return productLines[section].name
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as! ProductTableViewCell
-        let product = products[indexPath.row]
+        let product = productLines[indexPath.section].products[indexPath.row]
         cell.product = product
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+                let productLine = productLines[indexPath.section]
+                productLine.products.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
